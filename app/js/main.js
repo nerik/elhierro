91f01e008340bc71e31c059d6d750b476b565176
nerik <erik.escoffier@gmail.com>
2015-04-08 17:48:38 +0200

    scroll to Leaflet path test

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 var L = require('leaflet/dist/leaflet-src');
+var _ = require('underscore');
 
 var map = L.map('map', {
 	center: [27.7460, -18.0299],
@@ -33,33 +34,51 @@ var getXHRPromise = function(url) {
     });
 };
 
-getXHRPromise('./data/2/01_feet.json').then( data => {
-	console.log(data.features[0]);
 
-	var geojsonFeature = {
-	    "type": "Feature",
-	    "properties": {
-	        "name": "Coors Field",
-	        "amenity": "Baseball Stadium",
-	        "popupContent": "This is where the Rockies play!"
-	    },
-	    "geometry": {
-	        "type": "Point",
-	        "coordinates": [27.7460, -18.0299]
-	    }
-	};
-	var myLines = [{
-	    "type": "LineString",
-	    "coordinates": [[27.7460, -18.0299], [27.7460, -19.0299], [28.7460, -18.0299]]
-	}];
+var testCoords, testPath;
 
-	L.geoJson(data.features[0] , {
-		style: function (feature) {
-			return {color: '#FF0000', opacity: 1, dasharray: [5, 5] };
-		},
-		// onEachFeature: function (feature, layer) {
-		// 	layer.bindPopup(feature.properties.description);
-		// }
-	}).addTo(map);
-} );
+Promise.all([
+	getXHRPromise('./data/6/0_car.json'),
+	getXHRPromise('./data/6/1_feet.json'),
+	getXHRPromise('./data/6/2_car.json')
+]).then( data => {
+	//use Leaflet Omnivore w/Topojson instead??
+	//invert lat/lon
+	testCoords = data[0].features[0].geometry.coordinates.map( coords => [coords[1], coords[0]] );
+	testPath = L.polyline([], {color: 'red'}).addTo(map);
+
+	let feet1 = data[1].features[0].geometry.coordinates.map( coords => [coords[1], coords[0]] );
+	L.polyline(feet1, {color: 'blue', dashArray: '10,10'}).addTo(map);
+
+	let car2 = data[2].features[0].geometry.coordinates.map( coords => [coords[1], coords[0]] );
+	L.polyline([], {color: 'green'}).addTo(map);
+});
+
+
+var totalScroll = 2200;
+document.addEventListener('scroll', function (e) {
+	// console.log(window.scrollY);
+		
+
+	if (!testCoords) return;
+
+	var scrollR = window.scrollY/totalScroll;
+
+
+	testPath.setLatLngs( testCoords.slice(0, Math.floor( scrollR*testCoords.length ) ) );
+
+	console.log(window.scrollY);
+
+var elem = document.querySelector('.test');
+	if (window.scrollY>1700 && elem.style.position !== 'fixed') {
+		elem.style.top = elem.getBoundingClientRect().top + 'px';
+		elem.style.position = "fixed";
+		elem.style.width = "200px";
+	} else if (window.scrollY<=1700 && elem.style.position === 'fixed') {
+		elem.style.top = 0;
+		elem.style.position = "relative";
+	}
+});
+
+
 
